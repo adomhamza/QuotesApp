c88d2ddc93a814845bb8c5c45abe9dd88d56da0e
adomhamza <liveghost93@gmail.com>
2021-01-13 22:53:55 +0000

    Initial

--- lib/quote.dart
+++ lib/quote.dart
@@ -2,8 +2,8 @@ class Quote {
   String text;
   String author;
 
-  Quote({this.text, this.author}); //name auguments
-  //
+  Quote({this.text, this.author}); //name parameters
+  //OR like this below :
   // Quote(String text, String author) {
   //   this.text = text;
   //   this.author = author;
